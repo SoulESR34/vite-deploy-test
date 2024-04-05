462b972963b002094bfc0831891dcd9cfb67e312
Saul Esteban Santana Romero <siesaul1111@gmail.com>
2024-04-05 11:15:37 -0500

    prueba

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "https://SoulESR34.github.io/vite-deploy-test"
+})
